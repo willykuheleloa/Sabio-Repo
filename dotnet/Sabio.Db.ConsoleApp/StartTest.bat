dca1cccc1aad9032051fb8fefa94767646c76399
humphreysp12 <lefebagougis@gmail.com>
2022-09-30 09:34:21 -0700

    adjusted bat file filters and replaced the testing dll

--- dotnet/Sabio.Db.ConsoleApp/StartTest.bat
+++ dotnet/Sabio.Db.ConsoleApp/StartTest.bat
@@ -1,3 +1,3 @@
 SET IsWatcherDisabled=false
-:: dotnet test bin\Debug\net6.0\Sabio.Db.ConsoleApp.Tests.dll -l "trx;verbosity=detailed" -s "./default.runsettings" --filter "FullyQualifiedName~Friends | FullyQualifiedName~Users | FullyQualifiedName~Addresses"
-TestExecutables\Sabio.Db.ConsoleApp.TestResults.exe -a \\bin\\Debug\\net6.0 -d \\TestResults -t \\lib -f "FullyQualifiedName~Friends|FullyQualifiedName~Users|FullyQualifiedName~Addresses"
+:: dotnet test bin\Debug\net6.0\Sabio.Db.ConsoleApp.Tests.dll -l "trx;verbosity=detailed" -s "./default.runsettings" --filter "FullyQualifiedName~Friend | FullyQualifiedName~User | FullyQualifiedName~Address"
+TestExecutables\Sabio.Db.ConsoleApp.TestResults.exe -a \\bin\\Debug\\net6.0 -d \\TestResults -t \\lib -f "FullyQualifiedName~Friend|FullyQualifiedName~User|FullyQualifiedName~Address"
